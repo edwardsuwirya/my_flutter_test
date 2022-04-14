5b3f9dd43242eb7eba93c4054a2de3844c35792a
Edward Suwirya <edward_suwirya@yahoo.com>
2022-04-14 07:10:06 +0700

    Create group test

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,9 +2,22 @@ import 'package:my_flutter_test/counter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter valie should be incremented', () {
-    final counter = Counter();
-    counter.increment();
-    expect(counter.angka, 1);
+  group('Counter',(){
+    test('angka should start at 0', () {
+      expect(Counter().angka, 0);
+    });
+
+    test('Counter value should be incremented', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.angka, 1);
+    });
+
+    test('Counter value should be decremented', () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.angka, -1);
+    });
   });
+
 }
